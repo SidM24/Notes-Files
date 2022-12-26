900e8cadd25f234976d8efae122c13f40e40324d
Siddharth Maithani <siddharthmaithani94@gmail.com>
2022-12-26 20:17:53 +0530

    initial commit

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose')
+const mongoURI="mongodb://localhost:27017/?directConnection=true"
+mongoose.set('strictQuery',false)
+
+const connectToMongo =()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Successfully connected to mongoDB")
+    })
+}
+
+module.exports = connectToMongo

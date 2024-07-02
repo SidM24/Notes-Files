8d8ce0866b539513fdace3b2f18e85fc415f008e
Siddharth Maithani <siddharthmaithani94@gmail.com>
2024-07-02 20:24:58 +0530

    production

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 
 connectToMongo();
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 
 app.use(express.json({ limit: '50mb' }))
